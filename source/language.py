74b60a5f680e89e96f2eccf3bab164255edb1e7d
pavlvs-91 <gleindekp@gmail.com>
2025-10-19 12:59:08 +0200

    Implemented funcions operating on articles

--- source/language.py
+++ source/language.py
@@ -1,3 +1,4 @@
+# rzeczowniki
 def get_nouns():
     nouns = ["parde", "madre", "genitori", "fratello", "sorella", "marito", "moglie", "bambino", "bambina", "figlio",
              "figlia", "zio", "zia", "nonno", "nonna", "nipote", "cibo", "colazione", "pranzo", "cena", "pane", "formaggio",
@@ -13,6 +14,7 @@ def get_nouns():
     ]
     return nouns
 
+# przymiotniki
 def get_adjectives():
     adjectives = ["grande", "piccolo", "lungo", "corto", "buono", "cattivo", "freddo", "caldo", "bello", "bravo", "brutto",
                   "caro", "giovane", "vecchio", "nuovo", "stesso", "vero", "largo", "stretto", "alto", "basso", "pesante",
@@ -26,6 +28,7 @@ def get_adjectives():
                   "francese", "polaccho", "americano", "tedesco", "russo", "economico", "unico", "diverso", "reale"]
     return adjectives
 
+# czasowniki
 def get_verbs():
     verbs = ["essere", "avere", "fare", "dire", "capire", "sapere", "spiengare", "uscire", "andare", "venire",
              "preferire", "bere", "salire", "rimanere", "morire", "finire", "vedere", "leggere", "nascere",
@@ -40,6 +43,66 @@ def get_verbs():
              "giocare", "credere", "vendere", "ripetere", "godere", "dormire", "servire", "pregare", "partire", "pulire"]
     return verbs
 
+# czasowniki modalne
 def get_modal_verbs():
     modal_verbs = ["volere", "dovere", "potere"]
     return modal_verbs
+
+# liczba mnoga
+def get_plural(noun):
+    if noun in ("caffe", "te"):
+        return noun
+    elif noun[-1] == 'o':
+        return noun[:-1] + 'i'
+    elif noun[-1] == 'a':
+        return noun[:-1] + 'e'
+    elif noun[-1] == 'e':
+        return noun[:-1] + 'i'
+    else:
+        return noun
+
+# rodzajnik nieokreślony liczba pojedyncza
+def get_articolo_undeterminativo_singular(noun):
+    if noun[0] in ('x', 'z', 'y', 'j') or (noun[0] == 's' and noun[1] in ('t','p','c')) or (noun[0] == 'p' and noun[1] in ('n', 's')):
+        return "uno"
+    elif noun[-1] == 'a' and noun[0] in ('u','i','o','a','e'):
+        return "un'"
+    elif noun[-1] == 'a':
+        return "una"
+    else:
+        return "un"
+
+# rodzajnik nieokreślony liczba mnoga
+def get_articolo_undeterminativo_plural(noun):
+    if get_articolo_undeterminativo_singular(noun) == "uno":
+        return "degli"
+    elif get_articolo_undeterminativo_singular(noun) in ("una", "un'"):
+        return "delle"
+    elif get_articolo_undeterminativo_singular(noun) == "un" and noun[0] in ('u','i','o','a','e'):
+        return "degli"
+    else:
+        return "dei"
+
+# rodzajnik określony liczba pojedyncza
+def get_articolo_determinativo_singular(noun):
+    if noun[0] in ('x', 'z', 'y', 'j') or (noun[0] == 's' and noun[1] in ('t', 'p', 'c')) or (noun[0] == 'p' and noun[1] in ('n', 's')):
+        return "lo"
+    elif noun[0] in ('u','i','o','a','e'):
+        return "l'"
+    elif noun[-1] == 'a':
+        return "la"
+    else:
+        return "il"
+
+# rodzajnik określony liczba mnoga
+def get_articolo_determinativo_plural(noun):
+    if get_articolo_determinativo_singular(noun) == "lo":
+        return "gli"
+    elif get_articolo_determinativo_singular(noun) == "la":
+        return "le"
+    elif get_articolo_determinativo_singular(noun) == "l'" and noun[-1] == 'o':
+        return "gli"
+    elif get_articolo_determinativo_singular(noun) == "l'" and noun[-1] == 'a':
+        return "le"
+    else:
+        return "i"
