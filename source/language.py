d524766ceaa91f08bca09fab6a95f6b6f7a384fe
pavlvs-91 <gleindekp@gmail.com>
2025-10-19 11:49:53 +0200

    Created lists of +-100 most popular verbs, nouns and adjectives

--- source/language.py
+++ source/language.py
@@ -1,11 +1,45 @@
 def get_nouns():
-    nouns = ["dog", "cat"]
+    nouns = ["parde", "madre", "genitori", "fratello", "sorella", "marito", "moglie", "bambino", "bambina", "figlio",
+             "figlia", "zio", "zia", "nonno", "nonna", "nipote", "cibo", "colazione", "pranzo", "cena", "pane", "formaggio",
+             "vino", "caffe", "latte", "te", "torta", "panino", "carne", "sale", "insalata", "frutta", "verdura", "piatto",
+             "tavolo", "insegnante", "medico", "ingegnere", "cameriere", "commesso", "commessa", "segretario", "segretaria",
+             "giornale", "congresso", "parlamento", "bandiera", "banca", "contanti", "soldi", "negozio", "mercato", "maglietta",
+             "pantaloni", "scarpe", "borsa", "cappello", "cappotto", "vestito", "collana", "passatempi", "sport", "calcio",
+             "pallavolo", "tennis", "spiaggia", "libro", "film", "attore", "attrice", "musica", "casa", "cucina", "bagno",
+             "porta", "finestra", "scale", "letto", "orologio", "sapone", "specchio", "frigo", "macchina", "autobus",
+             "bicicletta", "treno", "aeroplano", "barca", "ufficio", "fabbrica", "collega", "computer", "gatto", "cane",
+             "pesce", "uccello", "cavallo", "leone", "tigre", "elefante", "orso", "natura", "albero", "fiore", "bosco",
+             "fiume", "montagna", "lago", "mare", "terra", "cielo", "sole", "luna", "stella" "vento", "pioggia"
+    ]
     return nouns
 
 def get_adjectives():
-    adjectives = ["small", "big"]
+    adjectives = ["grande", "piccolo", "lungo", "corto", "buono", "cattivo", "freddo", "caldo", "bello", "bravo", "brutto",
+                  "caro", "giovane", "vecchio", "nuovo", "stesso", "vero", "largo", "stretto", "alto", "basso", "pesante",
+                  "leggero", "vicino", "lontano", "fantastico", "terribile", "tiepido", "morbido", "duro", "gentile", "aperto",
+                  "chiuso", "divertente", "comico", "felice", "contento", "triste", "pazzo", "rapido", "lento", "facile",
+                  "difficile", "importante", "inutile", "utile", "rosso", "verde", "bianco", "rosso", "verde", "bianco",
+                  "nero", "giallo", "merrone", "arancione", "rosa", "viola", "blu", "rotondo", "quadrato", "sferico",
+                  "soleggiato", "nuvoloso", "dolce", "salato", "aspro", "acerbo", "acido", "amaro", "pericoloso", "interessante",
+                  "noioso", "forte", "debole", "malato", "ricco", "povero", "ordinato", "carino", "grasso", "magro", "elegante",
+                  "altro", "primo", "dritto", "possibile", "generale", "presente", "maggiore", "minore", "naturale", "italiano",
+                  "francese", "polaccho", "americano", "tedesco", "russo", "economico", "unico", "diverso", "reale"]
     return adjectives
 
 def get_verbs():
-    verbs = ["eat", "run"]
+    verbs = ["essere", "avere", "fare", "dire", "capire", "sapere", "spiengare", "uscire", "andare", "venire",
+             "preferire", "bere", "salire", "rimanere", "morire", "finire", "vedere", "leggere", "nascere",
+             "chiedere", "rispondere", "conoscere", "mettere", "aprire", "perdere", "vivere", "cuocere",
+             "scendere", "scrivere", "prendere", "rompere", "correre", "vincere", "tradurre", "chiudere",
+             "accendere", "crescere", "spingere", "chiamare", "scusare", "mancare", "interessare", "sembrare",
+             "amare", "odiare", "mangiare", "ascoltare", "parlare", "ordinare", "cercare", "pensare",
+             "incontrare", "portare", "toccare", "guardare", "tornare", "trovare", "dare", "abitare",
+             "sperare", "aspettare", "pranzare", "cenare", "telefonare", "comprare", "pagare", "entrare",
+             "lavorare", "studiare", "mandare", "uscire", "lasciare", "salutare", "usare", "visitare",
+             "camminare", "iniziare", "imparare", "cambiare", "guadagnare", "tirare", "invitare", "diventare",
+             "giocare", "credere", "vendere", "ripetere", "godere", "dormire", "servire", "pregare", "partire", "pulire"]
     return verbs
+
+def get_modal_verbs():
+    modal_verbs = ["volere", "dovere", "potere"]
+    return modal_verbs
